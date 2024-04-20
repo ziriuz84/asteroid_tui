c2ce103c9f1b8511fdbde3b5b78a2efb200acefd
Sirio Negri <ziriuz84@gmail.com>
2024-04-20 09:57:24 +0200

    Basic TUI with main menu and scheduling menu

--- src/ui.rs
+++ src/ui.rs
@@ -5,6 +5,8 @@ use ratatui::{
     Frame,
 };
 
+use ratatui::prelude::*;
+
 use crate::app::App;
 use crate::app::CurrentScreen;
 
@@ -14,12 +16,21 @@ pub fn render(app: &mut App, frame: &mut Frame) {
     // See the following resources:
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui-org/ratatui/tree/master/examples
+    let layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![
+            Constraint::Length(3),
+            Constraint::Length(3),
+            Constraint::Min(1),
+            Constraint::Length(3),
+        ])
+        .split(frame.size());
     match app.current_screen {
         CurrentScreen::MainMenu => {
-            render_main_menu(app, frame);
+            render_main_menu(app, frame, layout);
         }
         CurrentScreen::SchedulingMenu => {
-            render_scheduling_menu(app, frame);
+            render_scheduling_menu(app, frame, layout);
         }
         CurrentScreen::WeatherForecast => {
             render_weather_forecast(app, frame);
@@ -27,27 +38,117 @@ pub fn render(app: &mut App, frame: &mut Frame) {
     }
 }
 
-fn render_main_menu(app: &mut App, frame: &mut Frame) {
-    frame.render_widget(
-        Paragraph::new(format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            app.counter
-        ))
-        .block(
-            Block::bordered()
-                .title("Template")
-                .title_alignment(Alignment::Center)
-                .border_type(BorderType::Rounded),
-        )
-        .style(Style::default().fg(Color::Cyan).bg(Color::Black))
-        .centered(),
-        frame.size(),
-    )
+/// Centers a rect inside an area
+///
+/// * `r`: The area where to insert resulting rect
+/// * `percent_x`: width of resulting rect in percent of `r.width()`
+/// * `percent_y`: height of resulting rect in percent of `r.height()`
+fn centered_rect(r: Rect, percent_x: u16, percent_y: u16) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1]
 }
 
-fn render_scheduling_menu(app: &mut App, frame: &mut Frame) {}
+fn render_main_menu(
+    app: &mut App,
+    frame: &mut Frame,
+    layout: std::rc::Rc<[ratatui::layout::Rect]>,
+) {
+    frame.render_widget(
+        Paragraph::new("AsteroidTUI")
+            .block(
+                Block::bordered()
+                    //.title("Template")
+                    //.title_alignment(Alignment::Center)
+                    .border_type(BorderType::Rounded),
+            )
+            .style(Style::default().fg(Color::Red).bg(Color::Black))
+            .centered(),
+        layout[0],
+    );
+    frame.render_widget(
+        Paragraph::new("")
+            .block(Block::default())
+            .style(Style::default().bg(Color::Black)),
+        layout[1],
+    );
+    frame.render_widget(
+        Paragraph::new(
+            "Main Menu\n\
+        \n\n\
+        c - Configuration\n\
+        s - Scheduling\n\
+        q - quit",
+        )
+        .style(Style::default().fg(Color::Red).bg(Color::Black))
+        .centered(),
+        layout[2],
+    );
+    frame.render_widget(
+        Paragraph::new("Press q or Ctrl+C to quit")
+            .block(Block::bordered().border_type(BorderType::Rounded))
+            .style(Style::default().fg(Color::Red).bg(Color::Black))
+            .centered(),
+        layout[3],
+    );
+}
+
+fn render_scheduling_menu(
+    app: &mut App,
+    frame: &mut Frame,
+    layout: std::rc::Rc<[ratatui::layout::Rect]>,
+) {
+    frame.render_widget(
+        Paragraph::new("AsteroidTUI")
+            .block(
+                Block::bordered()
+                    //.title("Template")
+                    //.title_alignment(Alignment::Center)
+                    .border_type(BorderType::Rounded),
+            )
+            .style(Style::default().fg(Color::Red).bg(Color::Black))
+            .centered(),
+        layout[0],
+    );
+    frame.render_widget(
+        Paragraph::new("")
+            .block(Block::default())
+            .style(Style::default().bg(Color::Black)),
+        layout[1],
+    );
+    frame.render_widget(
+        Paragraph::new(
+            "Scheduling Menu\n\
+        \n\n\
+        w - Weather Forecast\n\
+        b - Back to Main Menu\n\
+        q - Quit",
+        )
+        .style(Style::default().fg(Color::Red).bg(Color::Black))
+        .centered(),
+        layout[2],
+    );
+    frame.render_widget(
+        Paragraph::new("Press q or Ctrl+C to quit")
+            .block(Block::bordered().border_type(BorderType::Rounded))
+            .style(Style::default().fg(Color::Red).bg(Color::Black))
+            .centered(),
+        layout[3],
+    );
+}
 
 fn render_weather_forecast(app: &mut App, frame: &mut Frame) {}
