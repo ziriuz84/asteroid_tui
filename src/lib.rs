4ae9ef125f1532ea68e820917bd5703810129d67
Sirio Negri <ziriuz84@gmail.com>
2024-10-09 06:03:43 +0200

    Removes all ratatui code

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-/// Application.
-pub mod app;
-
-/// Terminal events handler.
-pub mod event;
-
-/// Widget renderer.
-pub mod ui;
-
-/// Terminal user interface.
-pub mod tui;
-
-/// Event handler.
-pub mod handler;
-
 /// Settings Handler
 pub mod settings;
 
