0496c09d3aa653b6462f5a0e0b8ea885d193148a
Sirio Negri <ziriuz84@gmail.com>
2025-01-06 10:43:49 +0100

    wip

--- src/observing_target_list.rs
+++ src/observing_target_list.rs
@@ -25,3 +25,54 @@ pub struct PossibleTarget {
     /// Object altitude
     pub altitude: u8,
 }
+
+pub struct WhatsUpParams {
+    year: String,
+    month: String,
+    day: String,
+    hour: String,
+    minute: String,
+    duration: String,
+    max_objects: String,
+    min_alt: String,
+    solar_elong: String,
+    lunar_elong: String,
+    object_type: String,
+}
+
+pub fn get_observing_target_list(settings: &Settings, params: &WhatsUpParams) -> String {
+    let mut observing_target_list: Vec<PossibleTarget> = Vec::new();
+    let settings = Settings::new().unwrap();
+    let mut full_params: Vec<(&str, &str)> = Vec::new();
+    full_params.push(("utf8", "%E2%9C%93"));
+    full_params.push((
+        "authenticity_token",
+        "W5eBzzw9Clj4tJVzkz0z%2F2EK18jvSS%2BffHxZpAshylg%3D",
+    ));
+    full_params.push(("latitude", settings.get_latitude().to_string()).as_str());
+    full_params.push(("longitude", settings.get_longitude().to_string().as_str());
+    full_params.push(("year", params.year.as_str()));
+    full_params.push(("month", params.month.as_str()));
+    full_params.push(("day", params.day.as_str()));
+    full_params.push(("hour", params.hour.as_str()));
+    full_params.push(("minute", params.minute.as_str()));
+    full_params.push(("duration", params.duration.as_str()));
+    full_params.push(("max_objects", params.max_objects.as_str()));
+    full_params.push(("min_alt", params.min_alt.as_str()));
+    full_params.push(("solar_elong", params.solar_elong.as_str()));
+    full_params.push(("lunar_elong", params.lunar_elong.as_str()));
+    full_params.push(("object_type", params.object_type.as_str()));
+    full_params.push(("submit", "Submit"));
+    let url: reqwest::Url = reqwest::Url::parse_with_params(
+        "https://www.minorplanetcenter.net/whatsup/index",
+        full_params,
+    )
+    .unwrap();
+    let result = reqwest::blocking::get(url).unwrap().text();
+    result.unwrap()
+}
+
+pub fn parse_whats_up_response(response: &str) -> Result<Vec<PossibleTarget>> {
+    let data: Vec<PossibleTarget> = serde_json::from_str(response).unwrap();
+    Ok(data)
+}
